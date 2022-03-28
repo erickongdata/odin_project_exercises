5e92175c31109fcfefebbb5921a869a9677eebe3
Eric Kong <erickongdata@gmail.com>
2022-03-28 11:38:55 +0100

    Add reset button to Etch-A-Sketch

--- etch_a_sketch/index.js
+++ etch_a_sketch/index.js
@@ -1,12 +1,44 @@
+const body = document.querySelector("body");
+// Add reset button
+const resetBtn = document.createElement("button");
+resetBtn.classList.add("reset");
+resetBtn.textContent = "Reset";
+body.appendChild(resetBtn);
+resetBtn.addEventListener("click", resetBoard);
+
 // Add container to document
 const container = document.createElement("div");
-const body = document.querySelector("body");
 container.classList.add("container");
+container.setAttribute("style", "grid-template-columns: repeat(4, 1fr); grid-template-rows: repeat(4, 1fr);");
 body.appendChild(container);
+
 // Add 16 squares to container
-for (let i = 0; i < 16; i++) {
-  const square = document.createElement("div");
-  square.classList.add("square");
-  square.classList.add(`square${i}`);
-  container.appendChild(square);
+createSquares(4);
+
+// Add light square effect
+function lightSquare(square) {
+  square.classList.add("hover");
+}
+
+function resetBoard() {
+  const squares = document.querySelectorAll(".square");
+  squares.forEach((sqr) => sqr.classList.remove("hover"));
+  squares.forEach((sqr) => container.removeChild(sqr));
+  const size = prompt("Grid size? ");
+  container.setAttribute(
+    "style",
+    `grid-template-columns: repeat(${size}, 1fr); grid-template-rows: repeat(${size}, 1fr);`
+  );
+  createSquares(size);
+}
+
+function createSquares(size) {
+  for (let i = 0; i < size * size; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.classList.add(`square${i}`);
+    container.appendChild(square);
+  }
+  const squares = document.querySelectorAll(".square");
+  squares.forEach((sqr) => sqr.addEventListener("mouseover", () => lightSquare(sqr)));
 }
