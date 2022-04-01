7a04f116945500025efaf8b7a684645a1fc6251a
Eric Kong <erickongdata@gmail.com>
2022-04-01 12:25:07 +0100

    Add btn operator functions

--- calculator/index.js
+++ calculator/index.js
@@ -1,5 +1,5 @@
 // screen variables
-let displayInput = "0"; // main display
+let displayInput = ""; // main display
 let displayOperator = ""; // upper-right
 let displayResult = ""; // upper-left
 
@@ -20,7 +20,17 @@ function divide(a, b) {
 }
 
 function operate(operator, a, b) {
-  return operator(a, b);
+  return operator == "+"
+    ? add(a, b)
+    : operator == "-"
+    ? subtract(a, b)
+    : operator == "x"
+    ? multiply(a, b)
+    : divide(a, b);
+}
+
+function convertOperator(operator) {
+  return operator == "add" ? "+" : operator == "subtract" ? "-" : operator == "multiply" ? "x" : "÷";
 }
 
 function drawCalculator() {
@@ -99,7 +109,9 @@ function clearScreen() {
 }
 
 function getCalculatorState() {
-  // Return a code (1-4) that depends what display variables are present
+  // Return a code (1-6) that depends what display variables are present
+  if (displayInput == ".") return 6;
+  if (displayInput == "-") return 5;
   if (displayInput) {
     if (!displayOperator) return 2;
     return 4;
@@ -108,22 +120,18 @@ function getCalculatorState() {
   return 3;
 }
 
-function operatorConvert(displayOperator) {
-  return displayOperator == "+"
-    ? add()
-    : displayOperator == "-"
-    ? subtract()
-    : displayOperator == "x"
-    ? multiply()
-    : divide();
-}
-
 function calculator(input) {
   const state = getCalculatorState();
   console.log(input);
+  console.log("state" + state);
   switch (input) {
     case "clear":
       clearScreen();
+      break;
+    case "backspace":
+      if (displayInput) {
+        displayInput = displayInput.slice(0, -1);
+      }
       break;
     case "1":
     case "2":
@@ -137,25 +145,64 @@ function calculator(input) {
       // Append digit to main display
       if (displayInput != "0") {
         displayInput += input;
-        updateScreen();
         break;
       }
       // If current display is 0 digit - remove and append new digit
       displayInput = input;
-      updateScreen();
       break;
     case "equals":
-      const currOperator = operatorConvert(displayOperator);
-      if (state == 4) {
-        displayResult = operate(currOperator, displayResult, displayInput);
+      if (state === 6) break;
+      if (state === 4) {
+        displayResult = operate(displayOperator, +displayResult, +displayInput).toString();
         displayInput = displayResult;
         displayResult = "";
         displayOperator = "";
-        updateScreen();
-        break;
       }
       break;
+    case "period":
+      if (!displayInput.includes(".")) {
+        displayInput += ".";
+      }
+      break;
+    case "0":
+      if (displayInput) {
+        displayInput += "0";
+      }
+      break;
+    case "add":
+    case "subtract":
+    case "multiply":
+    case "divide":
+      if (state === 2) {
+        displayResult = displayInput;
+        displayOperator = convertOperator(input);
+        displayInput = "";
+        break;
+      }
+      if (state === 4) {
+        displayResult = operate(displayOperator, +displayResult, +displayInput).toString();
+        displayOperator = convertOperator(input);
+        displayInput = "";
+        break;
+      }
+      if (state === 3) {
+        if (input == "subtract") {
+          displayInput = "-";
+          break;
+        }
+        displayOperator = convertOperator(input);
+        break;
+      }
+      if (state === 1 && input == "subtract") {
+        displayInput = "-";
+        break;
+      }
+      if (state === 5 && input == "add") {
+        displayInput = "";
+        break;
+      }
   }
+  updateScreen();
 }
 
 drawCalculator();
