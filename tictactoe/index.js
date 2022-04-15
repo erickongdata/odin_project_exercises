f0c08010a38d21d40c642ff269a849a85055bcb9
Eric Kong <erickongdata@gmail.com>
2022-04-15 15:57:54 +0100

    Add activate to TicTacToe

--- tictactoe/index.js
+++ tictactoe/index.js
@@ -2,6 +2,11 @@
 const board = (() => {
   // Initialize board
   let board = [];
+  const markerRef = {
+    "_": "_",
+    "X": "X",
+    "O": "O",
+  };
 
   const reset = () => {
     board.length = 0; // clears array
@@ -15,8 +20,8 @@ const board = (() => {
     container.classList.add("container");
     for (let i = 0; i < 9; i++) {
       const div = document.createElement("div");
-      div.classList.add("square");
-      div.classList.add("square" + i);
+      div.classList.add("square", "square" + i);
+      div.dataset.value = i;
       container.append(div);
     }
     document.body.append(container);
@@ -25,9 +30,19 @@ const board = (() => {
   const printDisplay = () => {
     for (let i = 0; i < 9; i++) {
       const square = document.querySelector(`.square${i}`);
-      const mark = board[i] == "_" ? "_" : board[i] == "X" ? "X" : "O";
-      square.textContent = mark;
+      square.textContent = markerRef[board[i]];
     }
+  };
+
+  const activate = (marker) => {
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((sqr) =>
+      sqr.addEventListener("click", () => {
+        const index = sqr.dataset.value;
+        board[index] = marker;
+        sqr.textContent = markerRef[marker];
+      })
+    );
   };
 
   return {
@@ -35,13 +50,16 @@ const board = (() => {
     reset,
     createDisplay,
     printDisplay,
+    activate,
   };
 })();
 
 function game() {
+  const marker = "X";
   board.reset();
   board.createDisplay();
   board.printDisplay();
+  board.activate(marker);
   console.log(board.board);
 }
 
