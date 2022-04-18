baa0693bf63394bf24c72cc56ee1082bff205d73
Eric Kong <erickongdata@gmail.com>
2022-04-18 11:16:42 +0100

    Add Winner function to TTT

--- tictactoe/index.js
+++ tictactoe/index.js
@@ -10,14 +10,14 @@ const board = (() => {
     O: "O",
   };
 
-  const reset = () => {
+  function reset() {
     board.length = 0; // clears array
     for (let i = 0; i < 9; i++) {
       board.push("_");
     }
-  };
+  }
 
-  const createDisplay = () => {
+  function createDisplay() {
     const container = document.createElement("div");
     container.classList.add("container");
     for (let i = 0; i < 9; i++) {
@@ -27,16 +27,16 @@ const board = (() => {
       container.append(div);
     }
     document.body.append(container);
-  };
+  }
 
-  const printDisplay = () => {
+  function printDisplay() {
     for (let i = 0; i < 9; i++) {
       const square = document.querySelector(`.square${i}`);
       square.textContent = markerRef[board[i]];
     }
-  };
+  }
 
-  const activate = () => {
+  function activate() {
     const squares = document.querySelectorAll(".square");
     squares.forEach((sqr) =>
       sqr.addEventListener("click", () => {
@@ -45,17 +45,53 @@ const board = (() => {
         board[index] = marker;
         // Display marker on selected square
         sqr.textContent = markerRef[marker];
+        // CHECK FOR WIN
+        if (winner(index, marker)) {
+          console.log(`Win for ${marker}`)
+        };
+        // CHECK FULL BOARD
+        if (boardFull()) {
+          console.log("Game over");
+        }
         // Switch marker every time a square is chosen
         marker = marker == "X" ? "O" : "X";
 
-        // CHECK FOR WIN
-        // CHECK FULL BOARD
       })
     );
-  };
+  }
+
+  function boardFull() {
+    return !board.includes("_");
+  }
+
+  function winner(index, marker) {
+    // CHECK ROWS
+    const row_ind = Math.floor(index / 3);
+    const row = board.slice(row_ind * 3, (row_ind + 1) * 3);
+    if (row.every((sqr) => sqr == marker)) {
+      return true;
+    }
+    // CHECK COLUMNS
+    const col_ind = index % 3;
+    const col = [0, 1, 2].map((i) => board[col_ind + i * 3]);
+    if (col.every((sqr) => sqr == marker)) {
+      return true;
+    }
+    // CHECK SELECTED SQUARE IS ON A DIAGONAL, IF SO, THE DIAGONAL(S)
+    if (index % 2 == 0) {
+      const diagonal1 = [0, 4, 8].map((i) => board[i]);
+      if (diagonal1.every((sqr) => sqr == marker)) {
+        return true;
+      }
+      const diagonal2 = [2, 4, 6].map((i) => board[i]);
+      if (diagonal2.every((sqr) => sqr == marker)) {
+        return true;
+      }
+    }
+    return false;
+  }
 
   return {
-    board,
     reset,
     createDisplay,
     printDisplay,
@@ -69,7 +105,6 @@ function game() {
   board.createDisplay();
   board.printDisplay();
   board.activate();
-  console.log(board.board);
 }
 
 game();
