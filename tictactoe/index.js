c7ccfa8c4ef050598512ccf348672b8316a05d1f
Eric Kong <erickongdata@gmail.com>
2022-04-22 11:32:18 +0100

    Reorganize modules in TTT

--- tictactoe/index.js
+++ tictactoe/index.js
@@ -1,71 +1,43 @@
 // Initialize board
-let board = []; // board data is kept in an array (9 elements)
+let brd = []; // board data is kept in an array (9 elements)
 let marker = "X";
 let player = 1;
 // This is what is displayed for each marker
-const markerRef = {
+const markerSym = {
   _: "",
   X: "X",
   O: "O",
 };
 
+function switchPlayer() {
+  if (marker == "X") {
+    marker = "O";
+  } else marker = "X";
+
+  if (player == 1) {
+    player = 2;
+  } else {
+    player = 1;
+  }
+}
+
 // Board Module Pattern
-const boardMod = (() => {
+const board = (() => {
   function reset() {
-    board.length = 0; // clears array
+    brd.length = 0; // clears array
     for (let i = 0; i < 9; i++) {
-      board.push("_");
+      brd.push("_");
     }
   }
 
-  function activate() {
-    const squares = document.querySelectorAll(".square");
-    squares.forEach((sqr) =>
-      sqr.addEventListener("click", () => {
-        const index = sqr.dataset.value;
-        if (board[index] != "_") return;
-        // update board data
-        board[index] = marker;
-        // Display marker on selected square
-        sqr.textContent = markerRef[marker];
-        squares.forEach((sqr) => (sqr.disabled = true));
-        if (gameOver(index)) return;
-        switchPlayer();
-        // Computer move
-        setTimeout(() => {
-          const compMove = computer.simple();
-          board[compMove] = marker;
-          document.querySelector(`.square${compMove}`).textContent = markerRef[marker];
-          if (gameOver(compMove)) return;
-          switchPlayer();
-          squares.forEach((sqr) => (sqr.disabled = false));
-        }, 1000);
-      })
-    );
-  }
-
-  function switchPlayer() {
-    if (marker == "X") {
-      marker = "O";
-    } else marker = "X";
-
-    if (player == 1) {
-      player = 2;
-    } else {
-      player = 1;
-    }
-
-    display.updateStatus();
-  }
-
-  function gameOver(index) {
+  function isGameOver(index) {
     // CHECK FOR WIN
     if (winner(index)) {
       display.gameOverMessage("win");
       return true;
     }
     // CHECK IF BOARD IS FULL
-    if (!board.includes("_")) {
+    if (!brd.includes("_")) {
       display.gameOverMessage();
       return true;
     }
@@ -75,23 +47,23 @@ const boardMod = (() => {
   function winner(index) {
     // CHECK ROWS
     const row_ind = Math.floor(index / 3);
-    const row = board.slice(row_ind * 3, (row_ind + 1) * 3);
+    const row = brd.slice(row_ind * 3, (row_ind + 1) * 3);
     if (row.every((sqr) => sqr == marker)) {
       return true;
     }
     // CHECK COLUMNS
     const col_ind = index % 3;
-    const col = [0, 1, 2].map((i) => board[col_ind + i * 3]);
+    const col = [0, 1, 2].map((i) => brd[col_ind + i * 3]);
     if (col.every((sqr) => sqr == marker)) {
       return true;
     }
     // CHECK SELECTED SQUARE IS ON A DIAGONAL, IF SO, THE DIAGONAL(S)
     if (index % 2 == 0) {
-      const diagonal1 = [0, 4, 8].map((i) => board[i]);
+      const diagonal1 = [0, 4, 8].map((i) => brd[i]);
       if (diagonal1.every((sqr) => sqr == marker)) {
         return true;
       }
-      const diagonal2 = [2, 4, 6].map((i) => board[i]);
+      const diagonal2 = [2, 4, 6].map((i) => brd[i]);
       if (diagonal2.every((sqr) => sqr == marker)) {
         return true;
       }
@@ -100,7 +72,7 @@ const boardMod = (() => {
   }
 
   function availableSquares() {
-    return board.reduce((arr, value, index) => {
+    return brd.reduce((arr, value, index) => {
       if (value == "_") {
         arr.push(index);
       }
@@ -110,8 +82,8 @@ const boardMod = (() => {
 
   return {
     reset,
-    activate,
     availableSquares,
+    isGameOver,
   };
 })();
 
@@ -128,17 +100,17 @@ const display = (() => {
     const statusDisplay = document.createElement("div");
     statusDisplay.classList.add("status");
     document.body.append(container, statusDisplay);
-    updateStatus();
+    printStatus();
   }
 
   function printBoard() {
     for (let i = 0; i < 9; i++) {
       const square = document.querySelector(`.square${i}`);
-      square.textContent = markerRef[board[i]];
+      square.textContent = markerSym[brd[i]];
     }
   }
 
-  function updateStatus() {
+  function printStatus() {
     const statusDisplay = document.querySelector(".status");
     statusDisplay.textContent = `Player ${player} with ${marker}`;
   }
@@ -172,15 +144,15 @@ const display = (() => {
     // Reset board
     marker = "X";
     player = 1;
-    boardMod.reset();
+    board.reset();
     printBoard();
-    updateStatus();
+    printStatus();
   }
 
   return {
     createBoard,
     printBoard,
-    updateStatus,
+    printStatus,
     gameOverMessage,
     gameReset,
   };
@@ -189,7 +161,7 @@ const display = (() => {
 // Computer module pattern
 const computer = (() => {
   function simple() {
-    const available = boardMod.availableSquares();
+    const available = board.availableSquares();
     return available[Math.floor(Math.random() * available.length)]; // random
   }
   return {
@@ -197,12 +169,44 @@ const computer = (() => {
   };
 })();
 
+function computerMove() {
+  const squares = document.querySelectorAll(".square");
+  const compMove = computer.simple();
+  brd[compMove] = marker;
+  document.querySelector(`.square${compMove}`).textContent = markerSym[marker];
+  if (board.isGameOver(compMove)) return;
+  switchPlayer();
+  display.printStatus();
+  squares.forEach((sqr) => (sqr.disabled = false));
+}
+
 function game() {
   // Initialize
-  boardMod.reset();
+  board.reset();
   display.createBoard();
   display.printBoard();
-  boardMod.activate();
+  activateGame();
+
+  function activateGame() {
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((sqr) =>
+      sqr.addEventListener("click", () => {
+        const index = sqr.dataset.value;
+        if (brd[index] != "_") return;
+        // Player move
+        brd[index] = marker;
+        sqr.textContent = markerSym[marker];
+        squares.forEach((sqr) => (sqr.disabled = true));
+        if (board.isGameOver(index)) return;
+        switchPlayer();
+        display.printStatus();
+        // Computer move
+        setTimeout(() => {
+          computerMove();
+        }, 1000);
+      })
+    );
+  }
 }
 
 game();
